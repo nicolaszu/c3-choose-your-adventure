42166f830681049a6dc25fd9e893ce91d525df27
Nicolas Zuluaga <nicolaszuluaga@kpmg.ca>
2020-12-16 01:37:25 -0500

    removed purge for now

--- postcss.config.js
+++ postcss.config.js
@@ -6,28 +6,28 @@ module.exports = {
       stage: 0,
     }),
     require("tailwindcss")(),
-    IN_PRODUCTION &&
-      require("@fullhuman/postcss-purgecss")({
-        content: [`./public/**/*.html`, `./src/**/*.vue`],
-        defaultExtractor(content) {
-          const contentWithoutStyleBlocks = content.replace(
-            /<style[^]+?<\/style>/gi,
-            ""
-          );
-          return (
-            contentWithoutStyleBlocks.match(
-              /[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g
-            ) || []
-          );
-        },
-        whitelist: [],
-        whitelistPatterns: [
-          /-(leave|enter|appear)(|-(to|from|active))$/,
-          /^(?!(|.*?:)cursor-move).+-move$/,
-          /^router-link(|-exact)-active$/,
-          /^vs(__|--)/,
-        ],
-      }),
+    // IN_PRODUCTION &&
+    //   require("@fullhuman/postcss-purgecss")({
+    //     content: [`./public/**/*.html`, `./src/**/*.vue`],
+    //     defaultExtractor(content) {
+    //       const contentWithoutStyleBlocks = content.replace(
+    //         /<style[^]+?<\/style>/gi,
+    //         ""
+    //       );
+    //       return (
+    //         contentWithoutStyleBlocks.match(
+    //           /[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g
+    //         ) || []
+    //       );
+    //     },
+    //     whitelist: [],
+    //     whitelistPatterns: [
+    //       /-(leave|enter|appear)(|-(to|from|active))$/,
+    //       /^(?!(|.*?:)cursor-move).+-move$/,
+    //       /^router-link(|-exact)-active$/,
+    //       /^vs(__|--)/,
+    //     ],
+    //   }),
     require("autoprefixer")(),
   ],
 };
